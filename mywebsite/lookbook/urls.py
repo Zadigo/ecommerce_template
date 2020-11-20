a987bd028c8c64fb5a5fd047758ba852f17110a1
John Pendenque <pendenquejohn@gmail.com>
2020-11-20 11:36:09 +0100

    Code refactoring

--- mywebsite/lookbook/urls.py
+++ mywebsite/lookbook/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
+from lookbook import views
 
 app_name = 'lookbook'
 
 urlpatterns = [
-    
+    url(r'^lookbook$', views.LookBookView.as_view(), name='lookbook'),
 ]
--- mywebsite/lookbook/views.py
+++ mywebsite/lookbook/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class LookBookView(TemplateView):
+    """Base view for the website's lookbook"""
+    template_name = 'pages/lookbook.html'
