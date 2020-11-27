abe3d42a9fe2a35d602d5ec63b18a421e4ef3f82
John Pendenque <pendenquejohn@gmail.com>
2020-11-27 10:32:27 +0100

    Context for organization

--- mywebsite/mywebsite/organization.py
+++ mywebsite/mywebsite/organization.py
@@ -1,4 +1,33 @@
+from django.template import Context
 from legal.context_processors import Legal
+
+
+class Organization:
+    name = None
+    legal_name = None
+    brand = None
+
+    def __init__(self, **kwargs):
+        kwargs.update(
+            {
+                'name': self.name,
+                'legal_name': self.legal_name,
+                'brand': self.brand,
+                'website_name': self.name
+            }
+        )
+        self.kwargs = kwargs
+
+    def __call__(self, request):
+        c = Context(self.kwargs)
+        return c.flatten()
+
+    @classmethod
+    def get_context(cls, request):
+        instance = cls()
+        c = Context(instance.kwargs)
+        return c.flatten()
+
 
 class Nawoka(Legal):
     legal_name = 'Nawoka'
